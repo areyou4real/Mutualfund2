c64290f755b04c1eed10eed95ed2989b38baa2dd
areyou4real <119920294+areyou4real@users.noreply.github.com>
2025-07-23 11:51:48 +0530

    Update file_router.py

--- utils/file_router.py
+++ utils/file_router.py
@@ -1,39 +1,29 @@
-# utils/file_router.py
-
-import os
-
 from processors import (
-    adityabirla,
-    axis,
-    baroda,
-    hdfc,
-    hsbc,
-    icici,
-    mahindra,
-    mirae,
-    shriram,
-    sundaram,
+    adityabirla, axis, baroda, hdfc, hsbc,
+    icici, mahindra, mirae, shriram, sundaram
 )
 
-FUND_MAPPING = {
-    "adityabirla": adityabirla.process_adityabirla,
-    "axis": axis.process_axis,
-    "baroda": baroda.process_baroda,
-    "hdfc": hdfc.process_hdfc,
-    "hsbc": hsbc.process_hsbc,
-    "icici": icici.process_icici,
-    "mahindra": mahindra.process_mahindra,
-    "mirae": mirae.process_mirae,
-    "shriram": shriram.process_shriram,
-    "sundaram": sundaram.process_sundaram,
-}
-
-def route_file(filename: str):
-    """
-    Returns the processing function based on the fund name in filename.
-    """
-    base_name = os.path.basename(filename).lower()
-    for key in FUND_MAPPING:
-        if key in base_name:
-            return FUND_MAPPING[key]
-    return None
+def get_processor(file_name: str):
+    name = file_name.lower()
+    if "birla" in name:
+        return adityabirla.process_adityabirla
+    elif "axis" in name:
+        return axis.process_axis
+    elif "baroda" in name:
+        return baroda.process_baroda
+    elif "hdfc" in name:
+        return hdfc.process_hdfc
+    elif "hsbc" in name:
+        return hsbc.process_hsbc
+    elif "icici" in name:
+        return icici.process_icici
+    elif "mahindra" in name:
+        return mahindra.process_mahindra
+    elif "mirae" in name:
+        return mirae.process_mirae
+    elif "shriram" in name:
+        return shriram.process_shriram
+    elif "sundaram" in name:
+        return sundaram.process_sundaram
+    else:
+        raise ValueError("Unknown fund in file name: " + file_name)
